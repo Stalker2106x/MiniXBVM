e2ef6debca29d4ab1b4edee43e5277717925978b
Maxime MARTENS <contact@maximemartens.fr>
2020-09-30 00:03:56 +0200

    Removed electronic concept. (too heavy)

--- src/Electronic/Endpoint.cpp
+++
@@ -1,8 +0,0 @@
-#include "Electronic/Endpoint.hh"
-
-Endpoint Endpoint::Gnd = Endpoint("Gnd", Endpoint::Type::Output);
-
-Endpoint::Endpoint(std::string name_, Type type_ = Undefined)
-: name(name_), type(type_)
-{
-}
